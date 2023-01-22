343071505fdc970c28b89f6c8c4ba456bb2e3b9c
Kai Roth <kai@kana.jetzt>
2023-01-22 16:40:19 +0100

    ✔ added file and dir existence check utils

--- addons/mod_loader/mod_loader_utils.gd
+++ addons/mod_loader/mod_loader_utils.gd
@@ -222,6 +222,13 @@ static func get_json_string_as_dict(string: String) -> Dictionary:
 		return {}
 	return parsed.result
 
+static func is_file_existing(path: String) -> bool:
+	var file = File.new()
+	return file.file_exists(path)
+
+static func is_dir_existing(path: String) -> bool:
+	var dir = Directory.new()
+	return dir.dir_exists(path)
 
 # Get a flat array of all files in the target directory. This was needed in the
 # original version of this script, before becoming deprecated. It may still be
