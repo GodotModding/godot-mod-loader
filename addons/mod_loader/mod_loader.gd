4fcbb2ecec2fb72908aaa962f8214b7a29f702f6
Chris Bloomfield <43499897+ithinkandicode@users.noreply.github.com>
2023-02-13 14:19:07 +0000

    `get_mod_config` - remove a redundant bit of text

--- addons/mod_loader/mod_loader.gd
+++ addons/mod_loader/mod_loader.gd
@@ -557,7 +557,7 @@ func get_mod_config(mod_dir_name: String = "", key: String = "") -> Dictionary:
 	# Invalid mod ID
 	if not mod_data.has(mod_dir_name):
 		status_code = MLConfigStatus.INVALID_MOD_ID
-		status_msg = "ERROR - Mod ID was invalid: %s" % mod_dir_name
+		status_msg = "Mod ID was invalid: %s" % mod_dir_name
 
 	# Mod ID is valid
 	if status_code == MLConfigStatus.OK:
