ba95df6a16135ae07abbd60c3fb12b17350095b4
Kai Roth <kai@kana.jetzt>
2023-06-08 10:55:45 +0200

    fix: :bug: logs error if `ModLoaderStore` is not available
    closes #262

--- addons/mod_loader/internal/godot.gd
+++ addons/mod_loader/internal/godot.gd
@@ -39,19 +39,23 @@ static func check_autoload_order(autoload_name_before: String, autoload_name_aft
 # Check the index position of the provided autoload (0 = 1st, 1 = 2nd, etc).
 # Returns a bool if the position does not match.
 # Optionally triggers a fatal error
-static func check_autoload_position(autoload_name: String, position_index: int, trigger_error: bool = false) -> bool:
+static func check_autoload_position(autoload_name: String, position_index: int, trigger_error := false) -> bool:
 	var autoload_array := get_autoload_array()
 	var autoload_index := autoload_array.find(autoload_name)
 	var position_matches := autoload_index == position_index
 
 	if not position_matches and trigger_error:
-		var error_msg := "Expected %s to be the autoload in position %s, but this is currently %s." % [autoload_name, str(position_index + 1), autoload_array[position_index]]
-		var help_msg := ""
+		var error_msg := (
+			"Expected %s to be the autoload in position %s, but this is currently %s. "
+			% [autoload_name, str(position_index + 1), autoload_array[position_index]]
+		)
+		var help_msg := AUTOLOAD_CONFIG_HELP_MSG if OS.has_feature("editor") else ""
+		var final_message = error_msg + help_msg
 
-		if OS.has_feature("editor"):
-			help_msg = " To configure your autoloads, go to Project > Project Settings > Autoload."
-
-		ModLoaderLog.fatal(error_msg + help_msg, LOG_NAME)
+		push_error(final_message)
+		ModLoaderLog._write_to_log_file(final_message)
+		ModLoaderLog._write_to_log_file(JSON.print(get_stack(), "  "))
+		assert(false, final_message)
 
 	return position_matches
 
