bf11d2323a7480b76886595189ef5c328e9f7f6e
KANA <kai@kana.jetzt>
2023-06-14 15:26:48 +0200

    docs: :memo: extended comments for docs

--- addons/mod_loader/api/deprecated.gd
+++ addons/mod_loader/api/deprecated.gd
@@ -1,12 +1,20 @@
+# API methods for deprecating funcs. Can be used by mods with public APIs.
 class_name ModLoaderDeprecated
 extends Node
 
-# API methods for deprecating funcs. Can be used by mods with public APIs.
 
 const LOG_NAME := "ModLoader:Deprecated"
 
 
-# A method has changed its name/class
+# Marks a method that has changed its name or class.
+#
+# Parameters:
+# - old_method (String): The name of the deprecated method.
+# - new_method (String): The name of the new method to use.
+# - since_version (String): The version number from which the method has been deprecated.
+# - show_removal_note (bool): (optional) If true, includes a note about future removal of the old method. Default is true.
+#
+# Returns: void
 static func deprecated_changed(old_method: String, new_method: String, since_version: String, show_removal_note: bool = true) -> void:
 	_deprecated_log(str(
 		"DEPRECATED: ",
@@ -16,8 +24,15 @@ static func deprecated_changed(old_method: String, new_method: String, since_ver
 	))
 
 
-# A method has been entirely removed, with no replacement
-# (should never be needed but good to have just in case)
+# Marks a method that has been entirely removed, with no replacement.
+# Note: This should rarely be needed but is included for completeness.
+#
+# Parameters:
+# - old_method (String): The name of the removed method.
+# - since_version (String): The version number from which the method has been deprecated.
+# - show_removal_note (bool): (optional) If true, includes a note about future removal of the old method. Default is true.
+#
+# Returns: void
 static func deprecated_removed(old_method: String, since_version: String, show_removal_note: bool = true) -> void:
 	_deprecated_log(str(
 		"DEPRECATED: ",
@@ -27,15 +42,24 @@ static func deprecated_removed(old_method: String, since_version: String, show_r
 	))
 
 
-# Freeform deprecation message.
-# Allows you to add a deprecation comment without specifying the old/new method
+# Marks a method with a freeform deprecation message.
+#
+# Parameters:
+# - msg (String): The deprecation message.
+# - since_version (String): (optional) The version number from which the deprecation applies.
+#
+# Returns: void
 static func deprecated_message(msg: String, since_version: String = "") -> void:
 	var since_text := " (since version %s)" % since_version if since_version else ""
 	_deprecated_log(str("DEPRECATED: ", msg, since_text))
 
 
-# Internal func for logging with support to trigger warnings instead of fatal
-# errors
+# Internal function for logging deprecation messages with support to trigger warnings instead of fatal errors.
+#
+# Parameters:
+# - msg (String): The deprecation message.
+#
+# Returns: void
 static func _deprecated_log(msg: String) -> void:
 	if ModLoaderStore.ml_options.ignore_deprecated_errors or OS.has_feature("standalone"):
 		ModLoaderLog.warning(msg, LOG_NAME)
