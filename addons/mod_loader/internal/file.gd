9535e33338c956eeafdd3503548623c7b9ba9468
Mateu <57108338+MateuSai@users.noreply.github.com>
2023-08-26 12:57:56 +0200

    Fixed a bug where the _save_string_to_file function crashed if the directory can't be opened

--- addons/mod_loader/internal/file.gd
+++ addons/mod_loader/internal/file.gd
@@ -157,8 +157,8 @@ static func _save_string_to_file(save_string: String, filepath: String) -> bool:
 		"https://docs.godotengine.org/en/stable/classes/class_%40globalscope.html#enum-globalscope-error"
 	))
 
-	if not dir.dir_exists(file_directory):
-		var makedir_error := dir.make_dir_recursive(file_directory)
+	if not dir:
+		var makedir_error := DirAccess.make_dir_recursive_absolute(file_directory)
 		if not makedir_error == OK:
 			ModLoaderLog.fatal("Encountered an error (%s) when attempting to create a directory, with the path: %s" % [makedir_error, file_directory], LOG_NAME)
 			return false
