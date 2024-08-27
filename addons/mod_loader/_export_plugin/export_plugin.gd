72315d7fc9bc2545ee0e269737b8df9eec325308
Kai <kai@kana.jetzt>
2024-08-27 15:58:49 +0200

    refactor: :construction: rename vanilla functions
    Vanilla functions are renamed, and at the bottom of the script, mod loader hooks are generated. These hooks replace the vanilla functions and handle calls to the `callable_stack`.

--- addons/mod_loader/_export_plugin/export_plugin.gd
+++ addons/mod_loader/_export_plugin/export_plugin.gd
@@ -1,6 +1,8 @@
 extends EditorExportPlugin
 
 
+const METHOD_PREFIX := "GodotModLoader"
+
 func _get_name() -> String:
 	return "Godot Mod Loader Export Plugin"
 
@@ -16,93 +18,141 @@ func _export_file(path: String, type: String, features: PackedStringArray) -> vo
 	var new_script := GDScript.new()
 	new_script.source_code = source_code
 	new_script.reload()
-	
+
 	var method_store: Array[String] = []
-	
+
+	var mod_loader_hooks_start_string := """
+# ModLoader Hooks - The following code has been automatically added by the Godot Mod Loader export plugin.
+"""
+
+	source_code = "%s\n%s" % [source_code, mod_loader_hooks_start_string]
+
 	for method in new_script.get_script_method_list():
 		var method_first_line_start := get_index_at_method_start(method.name, source_code)
 		if method_first_line_start == -1 or method.name in method_store:
 			continue
-		var method_first_line_end := source_code.find("\n", method_first_line_start)
-		var tab_count := source_code.count("\t", method_first_line_start, method_first_line_end)
-		var tab_string := ""
+
+		var method_arg_string := get_function_parameters(method.name, source_code)
+		var mod_loader_hook_string := get_mod_loader_hook(method.name, method_arg_string, path)
+
 		# Store the method name
-		# Not sure if there is a way to get only the local methods in a script, 
-		# get_script_method_list() returns a full list, 
+		# Not sure if there is a way to get only the local methods in a script,
+		# get_script_method_list() returns a full list,
 		# including the methods from the scripts it extends,
 		# which leads to multiple entries in the list if they are overridden by the child script.
 		method_store.push_back(method.name)
-
-		for i in tab_count:
-			tab_string = "%s\t" % tab_string
-
-		source_code = source_code.insert(
-				method_first_line_start,
-				tab_string + "ModLoaderMod.call_from_callable_stack(self, '%s', '%s', true)\n" % [path, method.name]
-			)
-
-		var method_end_index := get_index_at_method_end(method.name, source_code)
-		source_code = source_code.insert(
-				method_end_index,
-				"\n" + tab_string + "ModLoaderMod.call_from_callable_stack(self, '%s', '%s', false)" % [path, method.name]
-			)
+		source_code = prefix_method_name(method.name, source_code)
+		source_code = "%s\n%s" % [source_code, mod_loader_hook_string]
 
 	skip()
 	add_file(path, source_code.to_utf8_buffer(), false)
 
 
-static func get_index_at_method_start(method_name: String, text: String) -> int:
+static func get_function_parameters(method_name: String, text: String) -> String:
+	# Regular expression to match the function definition with arbitrary whitespace
+	var pattern := "func\\s+" + method_name + "\\s*\\("
+	var regex := RegEx.new()
+	regex.compile(pattern)
+
+	# Search for the function definition
+	var result := regex.search(text)
+	if result == null:
+		return ""
+
+	# Find the index of the opening parenthesis
+	var opening_paren_index := result.get_end() - 1
+	if opening_paren_index == -1:
+		return ""
+
+	# Use a stack to match parentheses
+	var stack := []
+	var closing_paren_index := opening_paren_index
+	while closing_paren_index < text.length():
+		var char := text[closing_paren_index]
+		if char == '(':
+			stack.push_back('(')
+		elif char == ')':
+			stack.pop_back()
+			if stack.size() == 0:
+				break
+		closing_paren_index += 1
+
+	# If the stack is not empty, that means there's no matching closing parenthesis
+	if stack.size() != 0:
+		return ""
+
+	# Extract the substring between the parentheses
+	var param_string := text.substr(opening_paren_index + 1, closing_paren_index - opening_paren_index - 1)
+
+	# Remove all whitespace characters (spaces, newlines, tabs) from the parameter string
+	param_string = param_string.replace(" ", "")
+	param_string = param_string.replace("\n", "")
+	param_string = param_string.replace("\t", "")
+
+	return param_string
+
+
+static func get_method_arg_string(method_name: String, text: String) -> String:
 	var starting_index := text.find("func %s(" % method_name)
-	
-	if starting_index == -1:
-		return -1
-	
-	var method_start_index := text.find("\n", starting_index)
-
-	return method_start_index + 1
+	return ""
 
 
-# TODO: How do we handle returns?
-static func get_index_at_method_end(method_name: String, text: String) -> int:
-	var starting_index := text.rfind("func %s(" % method_name)
-	
-	if starting_index == -1:
-		return -1
+static func prefix_method_name(method_name: String, text: String, prefix := METHOD_PREFIX) -> String:
+	# Regular expression to match the function definition with arbitrary whitespace
+	var pattern := "func\\s+%s\\s*\\(" % method_name
+	var regex := RegEx.new()
+	regex.compile(pattern)
+
+	var result := regex.search(text)
+
+	if result:
+		return text.replace(result.get_string(), "func %s_%s(" % [prefix, method_name])
 	else:
-		# Start behind func
-		starting_index = starting_index + 5
+		print("WHAT?!")
+		return text
 
-	# Find the end of the method
-	var next_method_line_index := text.find("func ", starting_index)
-	var method_end := -1
 
-	if next_method_line_index == -1:
-		# Backtrack empty lines from the end of the file
-		method_end = text.length() -1
+static func get_mod_loader_hook(method_name: String, method_param_string: String, script_path: String, method_prefix := METHOD_PREFIX) -> String:
+	# Split parameters by commas
+	var param_list := method_param_string.split(",")
+
+	# Remove default values by splitting at '=' and taking the first part
+	for i in range(param_list.size()):
+		param_list[i] = param_list[i].split("=")[0]
+
+	# Join the cleaned parameter names back into a string
+	var arg_string := ",".join(param_list)
+	param_list = method_param_string.split(",")
+
+	# Remove types by splitting at ':' and taking the first part
+	for i in range(param_list.size()):
+		param_list[i] = param_list[i].split(":")[0]
+
+	arg_string = ",".join(param_list)
+
+	return """
+func {%METHOD_NAME%}({%METHOD_PARAMS%}):
+	ModLoaderMod.call_from_callable_stack(self, [{%METHOD_ARGS%}], "{%SCRIPT_PATH%}", "{%METHOD_NAME%}", true)
+	{%METHOD_PREFIX%}_{%METHOD_NAME%}({%METHOD_ARGS%})
+	ModLoaderMod.call_from_callable_stack(self, [{%METHOD_ARGS%}], "{%SCRIPT_PATH%}", "{%METHOD_NAME%}", false)
+""".format({
+		"%METHOD_PREFIX%": method_prefix,
+		"%METHOD_NAME%": method_name,
+		"%METHOD_PARAMS%": method_param_string,
+		"%METHOD_ARGS%": arg_string,
+		"%SCRIPT_PATH%": script_path,
+	})
+
+
+static func get_index_at_method_start(method_name: String, text: String) -> int:
+	# Regular expression to match the function definition with arbitrary whitespace
+	var pattern := "func\\s+%s\\s*\\(" % method_name
+	var regex := RegEx.new()
+	regex.compile(pattern)
+
+	var result := regex.search(text)
+
+	if result:
+		return text.find("\n", result.get_end())
 	else:
-		# Get the line before the next function line
-		method_end = text.rfind("\n", next_method_line_index)
-
-	# Backtrack to the last non-empty line
-	var last_non_empty_line_index := method_end
-	while last_non_empty_line_index > starting_index:
-		last_non_empty_line_index -= 1
-		# Remove spaces, tabs and newlines (whitespace) to check if the line really is empty
-		if text[last_non_empty_line_index].rstrip("\t\n "):
-			# Get the beginning of the line
-			var line_start_index := text.rfind("\n", last_non_empty_line_index) + 1
-			var line_end_index := text.find("\n", line_start_index)
-			# Check if the line declares a variable
-			if text.count("var ", line_start_index, line_end_index) > 0:
-				continue
-			if text.count("const ", line_start_index, line_end_index) > 0:
-				continue
-			# Check if the last line is a top level return
-			var current_line := text.substr(line_start_index, line_end_index - line_start_index)
-			print(current_line)
-			if current_line.begins_with("\treturn "):
-				continue
-			
-			break # encountered a filled line
-
-	return last_non_empty_line_index +1
+		return -1
