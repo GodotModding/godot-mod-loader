979991ae16c4c8522d9678fe009e2684084b7cc3
Qubus0 <steen.rickmer@gmx.de>
2024-09-06 10:32:27 +0200

    :construction: fix: typos

--- addons/mod_loader/internal/mod_hook_packer.gd
+++ addons/mod_loader/internal/mod_hook_packer.gd
@@ -1,7 +1,7 @@
 extends Node
 
 
-const ModHookPreprocessorScript := preload("res://addons/mod_loader/_export_plugin/mod_hook_preprocessor.gd")
+const ModHookPreprocessorScript = preload("res://addons/mod_loader/internal/mod_hook_preprocessor.gd")
 static var ModHookPreprocessor
 
 
@@ -20,22 +20,21 @@ func run_script() -> void:
 	if OS.has_feature("editor"):
 		res = ProjectSettings.globalize_path("res://").rsplit("/", true, 2)[0]
 
-	var save_base_path := res.path_join("godot_mod_loader/temp_test_mod/")
-	print(save_base_path)
+	var save_base_path := res.path_join("godot_mod_loader/")
+	prints("Saved to:", save_base_path)
 	DirAccess.make_dir_recursive_absolute(save_base_path)
 
 	var zip_writer := ZIPPacker.new()
-	var err := zip_writer.open(save_base_path + "temp_test_mod.zip")
+	var err := zip_writer.open(save_base_path.path_join("temp_test_mod.zip"))
 	if err != OK:
 		printerr(err)
 
-	transform_scripts_recursive(ModHookPreprocessor.process_script, zip_writer, save_base_path)
+	transform_scripts_recursive(ModHookPreprocessor.process_script, zip_writer)
 
 	zip_writer.close()
-	ZIPPacker
 
 
-func transform_scripts_recursive(callback: Callable, zip_writer: ZIPPacker, save_base: String, path := "res://") -> void:
+func transform_scripts_recursive(callback: Callable, zip_writer: ZIPPacker, path := "res://") -> void:
 	var dir := DirAccess.open(path)
 	if not dir:
 		printt("An error occurred when trying to access the path:", path)
@@ -49,7 +48,7 @@ func transform_scripts_recursive(callback: Callable, zip_writer: ZIPPacker, save
 			continue
 
 		if dir.current_is_dir():
-			transform_scripts_recursive(callback, zip_writer, save_base, dir.get_current_dir() + file_name + "/")
+			transform_scripts_recursive(callback, zip_writer, dir.get_current_dir() + file_name + "/")
 			file_name = dir.get_next()
 			continue
 
