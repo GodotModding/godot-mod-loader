d136db4395eee4af228cb7d34f1e6ea779be0341
Kai <kai@kana.jetzt>
2025-02-05 17:18:20 +0100

    test: :test_tube: added no callable set test

--- test/Unit/test_options.gd
+++ test/Unit/test_options.gd
@@ -18,6 +18,18 @@ func test_customize_script() -> void:
 	)
 
 
+func test_customize_script_no_callable() -> void:
+	# Clear saved error logs before testing to prevent false positives.
+	ModLoaderLog.logged_messages.by_type.error.clear()
+
+	ModLoaderStore._update_ml_options_from_options_resource("res://test_options/customize_script_no_callable_set/options_custom_validation_no_callable_set.tres")
+	var manifest := load_manifest_test_mod_1()
+
+	var logs := ModLoaderLog.get_by_type_as_string("error")
+
+	assert_string_contains("".join(logs), "No custom game version validation callable detected. Please provide a valid validation callable.")
+
+
 func test_game_verion_validation_disabled() ->  void:
 	ModLoaderStore._update_ml_options_from_options_resource("res://test_options/game_version_validation_disabled/options_game_version_validation_disabled.tres")
 	var manifest := load_manifest_test_mod_1()
