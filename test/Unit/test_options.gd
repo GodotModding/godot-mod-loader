8d12b7a17dc573313b45fae398c48365a58e9b1b
Kai <kai@kana.jetzt>
2025-02-05 10:25:43 +0100

    test: :test_tube: added `test_game_verion_validation_default`

--- test/Unit/test_options.gd
+++ test/Unit/test_options.gd
@@ -22,3 +22,16 @@ func test_game_verion_validation_disabled() ->  void:
 	var manifest := ModManifest.new(manifest_data, mod_path)
 
 	assert_true(manifest.validation_messages_error.size() == 0)
+
+
+func test_game_verion_validation_default() ->  void:
+	ModLoaderStore._update_ml_options_from_options_resource("res://test_options/game_version_validation_default/options_game_version_validation_default.tres")
+	# Load manifest file
+	var mod_path := "res://mods-unpacked/test-mod1/"
+	var manifest_data: Dictionary = _ModLoaderFile.load_manifest_file(mod_path)
+	var manifest := ModManifest.new(manifest_data, mod_path)
+
+	assert_eq(
+		manifest.validation_messages_error,
+		["The mod \"test-mod1\" is incompatible with the current game version.\r\n\t\t\t(current game version: 1000.0.0, mod compatible with game versions: [\"0.0.1\"])"]
+	)
