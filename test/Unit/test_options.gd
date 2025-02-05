c26a4bf761ed4aaa8f1367738e99095cac4f83ac
Kai <kai@kana.jetzt>
2025-02-05 11:58:47 +0100

    refactor: :recycle: clean up a bit

--- test/Unit/test_options.gd
+++ test/Unit/test_options.gd
@@ -1,12 +1,16 @@
 extends GutTest
 
 
-func test_customize_script() -> void:
-	ModLoaderStore._update_ml_options_from_options_resource("res://test_options/customize_script/options_custom_validation.tres")
-	# Load manifest file
+func load_manifest_test_mod_1() -> ModManifest:
 	var mod_path := "res://mods-unpacked/test-mod1/"
 	var manifest_data: Dictionary = _ModLoaderFile.load_manifest_file(mod_path)
-	var manifest := ModManifest.new(manifest_data, mod_path)
+
+	return ModManifest.new(manifest_data, mod_path)
+
+
+func test_customize_script() -> void:
+	ModLoaderStore._update_ml_options_from_options_resource("res://test_options/customize_script/options_custom_validation.tres")
+	var manifest := load_manifest_test_mod_1()
 
 	assert_eq(
 		"".join(manifest.validation_messages_warning),
@@ -16,20 +20,14 @@ func test_customize_script() -> void:
 
 func test_game_verion_validation_disabled() ->  void:
 	ModLoaderStore._update_ml_options_from_options_resource("res://test_options/game_version_validation_disabled/options_game_version_validation_disabled.tres")
-	# Load manifest file
-	var mod_path := "res://mods-unpacked/test-mod1/"
-	var manifest_data: Dictionary = _ModLoaderFile.load_manifest_file(mod_path)
-	var manifest := ModManifest.new(manifest_data, mod_path)
+	var manifest := load_manifest_test_mod_1()
 
 	assert_true(manifest.validation_messages_error.size() == 0)
 
 
 func test_game_verion_validation_default() ->  void:
 	ModLoaderStore._update_ml_options_from_options_resource("res://test_options/game_version_validation_default/options_game_version_validation_default.tres")
-	# Load manifest file
-	var mod_path := "res://mods-unpacked/test-mod1/"
-	var manifest_data: Dictionary = _ModLoaderFile.load_manifest_file(mod_path)
-	var manifest := ModManifest.new(manifest_data, mod_path)
+	var manifest := load_manifest_test_mod_1()
 
 	assert_eq(
 		"".join(manifest.validation_messages_error).replace("\r", "").replace("\n", "").replace("\t", ""),
