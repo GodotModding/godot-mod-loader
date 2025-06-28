85b3b65add4936dc875db6ad2ea9fef856c49887
Dumb Bird <72983221+ZackeryRSmith@users.noreply.github.com>
2025-06-28 06:40:31 -1100

    bug: fix editor feature check
    doesn't error when ModLoaderStore isn't initialized

--- addons/mod_loader/api/log.gd
+++ addons/mod_loader/api/log.gd
@@ -403,7 +403,8 @@ static func get_all_entries_as_string(log_entries: Array) -> Array:
 # =============================================================================
 
 static func _print_rich(prefix: String, message: String, color: Color, bold := true) -> void:
-	if ModLoaderStore.has_feature.editor:
+	var in_editor: bool = OS.has_feature("editor") if not ModLoaderStore else ModLoaderStore.has_feature.editor
+	if in_editor:
 		var prefix_text := "[b]%s[/b]" % prefix if bold else prefix
 		print_rich("[color=%s]%s[/color]%s" % [
 			color.to_html(false),
